3df01823a10792a5131359e92c3b61bf28fbc8fa
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2025-01-10 22:38:06 +0300

    Create temel_operasyonlar.js

---
+++ temel_operasyonlar.js
@@ -0,0 +1,13 @@
+// var toplam = sayi1 + sayi2
+// undefined
+// toplam
+// '23'
+
+// Number fonksiyonu = metinsel ifadeyi sayısal ifadeye çevirir.
+
+// var toplam = Number(sayi1) + Number(sayi2)
+// undefined
+// toplam
+// 5
+
+//#######################################################################################################
