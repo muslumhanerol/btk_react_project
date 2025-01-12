0cecc8141719f77622ce830dbf4938fd14c11f96
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2025-01-12 21:45:15 +0300

    diziler

--- 02_fonksiyonlar.js
+++ 02_fonksiyonlar.js
@@ -8,3 +8,12 @@ selamVer()
 //#####################################################################################################
 
 //  return ifadeli fonksiyonlar.
+
+function topla(a, b) {
+    return a + b
+}
+undefined
+topla(2, 3)
+5
+
+
