73fe469ca627d00ce4588d667d5fc9f6ef6c281c
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2025-03-01 00:34:04 +0300

    Update ileri_diziler.js

--- 12_ileri_diziler/ileri_diziler.js
+++ 12_ileri_diziler/ileri_diziler.js
@@ -31,9 +31,44 @@
 //map fonksiyonu. Tek bir satırda yazılmalı aksi halde undefined hatası alırsın.
 
 
+// const dizi = [1, 2, 3, 4]
+
+// const mapDizi = dizi.map(sayi => sayi * 3)
+
+// console.log(mapDizi);
+
+
+// const dizi = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const mapSirala = dizi.map(sayi => sayi)
+// console.log(mapSirala);
+
+//#################################################################################
+
+// filter fonksiyonu.
+
+// const dizi = [1, 2, 3, 4]
+
+// const filterDizi = dizi.filter(sayi => sayi > 2) //sadece 2 den büyük olanları yazdırır.
+// console.log(filterDizi);
+
+
+// const mapDizi = dizi.map(sayi => sayi > 2) // 2 den büyük olanlar true küçük olanlar false olarak gösterilir.
+// console.log(mapDizi);
+
+//#################################################################################
+
+// reduce fonksiyonu. sum denilen kümülatif toplama işlemi gerçekleşir.
 const dizi = [1, 2, 3, 4]
 
-const mapDizi = dizi.map(sayi => sayi * 3)
+const toplam = dizi.reduce((acc, sayi) => { //acc varsayılan değeri 0 dır toplama sıfırdan bailar.
+    return acc + sayi;
+})
 
-console.log(mapDizi);
+const toplam2 = dizi.reduce((acc, sayi) => { //acc başlanğıç değerini 10 dan başlattık.
+    return acc + sayi;
+}, 10)
+
+console.log(toplam2);
+
 
