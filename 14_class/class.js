1e4da2bb8ded0a8cebe2bc26b7c07e8d45e18889
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2025-03-01 22:39:00 +0300

    Update class.js

--- 14_class/class.js
+++ 14_class/class.js
@@ -1,2 +1,25 @@
-// Sürdürülebilir yazılımlar geliştirmek için.
+// Sürdürülebilir yazılımlar geliştirmek için. Class lar Pascal Case yazılır yani ilk harf büyük.
+
+// this.ad Personel içerisine ad diye birşey ekliyor.
+
+class Personel {
+
+    constructor(ad, soyad) {
+        this.ad = ad;
+        this.soyad = soyad;
+    }
+
+    kaydet() {
+        console.log("Personel kaydedildi." + this.ad);
+    }
+}
+
+const personel = new Personel("Muslum", "Erol") //Bietane class örneği oluşturdum.
+
+personel.kaydet();
+
+personel.ad = "Muslum"
+console.log(personel.ad);
+
+
 
